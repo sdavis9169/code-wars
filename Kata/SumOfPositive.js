2114098b4d719b8cdb5a2aac5306bbfb9729f38a
Sdavis <sdavis@Stevens-MacBook-Pro.local>
2019-04-12 14:14:32 -0600

    Training

--- Kata/SumOfPositive.js
+++ Kata/SumOfPositive.js
@@ -4,9 +4,10 @@ function positiveSum(arr) {
     let sum = 0;
     for(let i = 0; i < arr.length; i++){
         if(arr[i] > 0){
-            return sum += arr[i]
-        }
-    }return positiveSum
+console.log(arr[i])
+sum += arr[i]
+        } 
+    } return sum
 }
 
 console.log(positiveSum([1,-2,3,4,5]))//13;
